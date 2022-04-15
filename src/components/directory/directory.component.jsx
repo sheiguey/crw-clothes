76e470c71c28e7faed66ae0d2b9abb292a31ff31
unknown <fshiti@amhconsulting-group.com>
2022-04-15 12:49:27 +0100

    styled all components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,33 +13,38 @@ class Directory extends Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'shop/hats',
+                  size : 'large'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: 'shop/jackets',
+                  size: 'large'
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: 'shop/sneakers',
+                  size: 'large'
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: 'shop/womens',
+                  size: 'large'
                 },
                 {
                   title: 'mens',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: 'shop/mens',
+                  size: 'large' 
                 }
               ],
 
@@ -54,16 +59,16 @@ class Directory extends Component {
 
     render() {
         return (
-            <div>
+          
                <div className='directory-menu'>
       
-                 {this.state.sections.map(({title,imageUrl,id})=>(
-                   <MenuItem key={id} title={title}/>
+                 {this.state.sections.map(({title,imageUrl,id,size})=>(
+                   <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
                  )
 
                  )}
                </div>
-            </div>
+          
         );
     }
 }
